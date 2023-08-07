f638593c8636f6d9a4ba8ebd6df0cb7fd3bb7f2e
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-08-07 20:24:14 -0300

    Removendo pessoa com DELETE

--- src/main/java/com/algamoney/api/controller/PessoaController.java
+++ src/main/java/com/algamoney/api/controller/PessoaController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,6 +46,12 @@ public class PessoaController {
 		return pessoaRepository.findById(codigo)
 				 .map(ResponseEntity::ok)
 				 .orElse(ResponseEntity.notFound().build());
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Long codigo) {
+		pessoaRepository.deleteById(codigo);
 	}
 	
 }
