3606755ed3aed3106354a72f6dd4f007e472ad41
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-08-08 20:50:03 -0300

    Desafio: Cadastrando o primeiro lançamento

--- src/main/java/com/algamoney/api/controller/LancamentoController.java
+++ src/main/java/com/algamoney/api/controller/LancamentoController.java
@@ -2,13 +2,21 @@ package com.algamoney.api.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algamoney.api.event.RecursoCriadoEvent;
 import com.algamoney.api.model.Lancamento;
 import com.algamoney.api.repository.LancamentoRepository;
 
@@ -18,6 +26,9 @@ public class LancamentoController {
 
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 	
 	
 	@GetMapping
@@ -30,5 +41,15 @@ public class LancamentoController {
 		return lancamentoRepository.findById(codigo)
 				.map(ResponseEntity::ok)
 				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Lancamento cadastrarLancamento(@RequestBody Lancamento lancamento, HttpServletResponse response) {
+		lancamento = lancamentoRepository.save(lancamento);
+		
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamento.getCodigo()));
+		
+		return lancamento;
 	}
 }
