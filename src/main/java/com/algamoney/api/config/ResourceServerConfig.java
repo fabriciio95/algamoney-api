269c48dcfe3f088030906298066ce9a03f5359c0
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-08-10 21:13:28 -0300

    Implementando segurança com OAuth 2 e Password Flow

--- src/main/java/com/algamoney/api/config/ResourceServerConfig.java
+++ src/main/java/com/algamoney/api/config/ResourceServerConfig.java
@@ -1,21 +1,26 @@
 package com.algamoney.api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+	
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
-		    .withUser("admin").password("{noop}admin").roles("ROLE");
+		    .withUser("admin").password("admin").roles("ROLE");
 	}
 	
 	@Override
@@ -24,10 +29,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		    	.antMatchers("/categorias").permitAll()
 		    	.anyRequest().authenticated()
 		    .and()
-		    	.httpBasic()
-		    .and()
 		        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		    .and()
-		        .csrf().disable();
+		        .csrf().disable()
+		        .oauth2ResourceServer().opaqueToken();
+	}
+	
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return NoOpPasswordEncoder.getInstance();
 	}
 }
