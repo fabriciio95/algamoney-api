410db991b29575fcf7c3a13ed29c723a784e860c
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-11-08 21:28:04 -0300

    Buscando lançamentos vencidos com Spring Data JPA

--- src/main/java/com/algamoney/api/repository/UsuarioRepository.java
+++ src/main/java/com/algamoney/api/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package com.algamoney.api.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +11,7 @@ import com.algamoney.api.model.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
-	public Optional<Usuario> findByEmail(String email);
+	Optional<Usuario> findByEmail(String email);
+	
+	List<Usuario> findByPermissoesDescricao(String permissaoDescricao);
 }
