3d0a6d27e6558e680b175aa1d38b958d8633dc35
Fabricio Macedo <fabriciusiqueira@gmail.com>
2023-11-09 20:48:06 -0300

    Criando a entidade Contato para suportar mestre-detalhe

--- src/main/java/com/algamoney/api/model/Contato.java
+++ src/main/java/com/algamoney/api/model/Contato.java
@@ -1,40 +1,38 @@
 package com.algamoney.api.model;
 
-import java.util.List;
 import java.util.Objects;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Transient;
-import javax.validation.Valid;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 @Entity
-public class Pessoa {
+public class Contato {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	
-	@NotNull
+	@NotBlank
 	private String nome;
 	
+	@Email
 	@NotNull
-	private Boolean ativo;
+	private String email;
 	
-	@Embedded
-	private Endereco endereco;
+	@NotBlank
+	private String telefone;
 	
-	@Valid
-	@OneToMany(mappedBy = "pessoa", cascade = CascadeType.ALL)
-	private List<Contato> contatos;
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "codigo_pessoa")
+	private Pessoa pessoa;
 
 	public Long getCodigo() {
 		return codigo;
@@ -52,36 +50,30 @@ public class Pessoa {
 		this.nome = nome;
 	}
 
-	public Boolean getAtivo() {
-		return ativo;
+	public String getEmail() {
+		return email;
 	}
 
-	public void setAtivo(Boolean ativo) {
-		this.ativo = ativo;
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
-	public Endereco getEndereco() {
-		return endereco;
+	public String getTelefone() {
+		return telefone;
 	}
 
-	public void setEndereco(Endereco endereco) {
-		this.endereco = endereco;
-	}
-	
-	public List<Contato> getContatos() {
-		return contatos;
-	}
-	
-	public void setContatos(List<Contato> contatos) {
-		this.contatos = contatos;
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
 	}
 
-	@JsonIgnore
-	@Transient
-	public boolean isInativo() {
-		return !ativo;
+	public Pessoa getPessoa() {
+		return pessoa;
 	}
-	
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(codigo);
@@ -95,7 +87,7 @@ public class Pessoa {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Pessoa other = (Pessoa) obj;
+		Contato other = (Contato) obj;
 		return Objects.equals(codigo, other.codigo);
 	}
 }
